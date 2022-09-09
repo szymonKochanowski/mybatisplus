92cbd5635a3602fadd01ed57c7818fe45ad2722b
Szymon Kochanowski <szymon.kochanowski@fdmgroup.com>
2022-09-09 12:46:22 +0200

    Added one class Draw with task regarding print stars :)

--- src/main/java/com/springbootmybatis/SpringBootMybatisApplication.java
+++ src/main/java/com/springbootmybatis/SpringBootMybatisApplication.java
@@ -1,7 +1,11 @@
 package com.springbootmybatis;
 
+import com.springbootmybatis.service.Stars;
+import com.springbootmybatis.service.impl.Draw;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.springbootmybatis.mapper")
@@ -10,6 +14,20 @@ public class SpringBootMybatisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMybatisApplication.class, args);
+
+
+		Draw draw = new Draw();
+
+		System.out.println("Square: n = 2");
+		System.out.println(draw.drawSquare(2));
+		System.out.println("Rectangle: n = 2, m = 3");
+		System.out.println(draw.drawRectangle(2, 3));
+		System.out.println("Draw Isosceles Triangle: n = 4");
+		System.out.println(draw.drawIsoscelesTriangle(4));
+		System.out.println("Draw Diamond: n = 5");
+		System.out.println(draw.drawDiamond(5));
+		System.out.println("Draw Rectangle Triangle: n = 4");
+		System.out.println(draw.drawRectangleTriangle(4))   ;
 	}
 
 }
--- src/main/java/com/springbootmybatis/service/impl/Draw.java
+++ src/main/java/com/springbootmybatis/service/impl/Draw.java
@@ -4,28 +4,95 @@ import com.springbootmybatis.service.Stars;
 
 public class Draw implements Stars {
 
+    private static final String STAR = "*";
+    private static final String DASH = "-";
+    private static final String NEW_LINE = "\n";
+
+
     @Override
     public String drawSquare(int n) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                stringBuilder.append(STAR);
+            }
+            stringBuilder.append(NEW_LINE);
+        }
+        return stringBuilder.toString();
     }
 
     @Override
     public String drawRectangle(int n, int m) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            for (int z = 0; z < n; z++) {
+                stringBuilder.append(STAR);
+            }
+            stringBuilder.append(NEW_LINE);
+        }
+        return stringBuilder.toString();
     }
 
     @Override
     public String drawIsoscelesTriangle(int n) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        int amountStarsInRow = 1;
+        int amountDashInRow = n - 1;
+        for (int i = 0; i < n; i++) {
+            for (int z = 0; z < amountDashInRow; z++) {
+                stringBuilder.append(DASH);
+            }
+            for (int s = 0; s < amountStarsInRow; s++) {
+                stringBuilder.append(STAR);
+            }
+            amountDashInRow -= 1;
+            amountStarsInRow += 2;
+            stringBuilder.append(NEW_LINE);
+        }
+        return stringBuilder.toString();
     }
 
     @Override
     public String drawDiamond(int n) {
-        return null;
+        isOddNumber(n);
+        int numberOfDownRows = n / 2;
+        String upPartOfDiamond = drawIsoscelesTriangle(Integer.parseInt(String.valueOf(n / 2 + 1)));
+        StringBuilder downPartOfDiamnond = new StringBuilder(upPartOfDiamond);
+        int amountStarsInRow = n - 2;
+        int amountDashInRow = 1;
+        for (int i = 0; i < numberOfDownRows; i++) {
+            for (int z = 0; z < amountDashInRow; z++) {
+                downPartOfDiamnond.append(DASH);
+            }
+            for (int s = 0; s < amountStarsInRow; s++) {
+                downPartOfDiamnond.append(STAR);
+            }
+            amountDashInRow += 1;
+            amountStarsInRow -= 2;
+            downPartOfDiamnond.append(NEW_LINE);
+        }
+        return downPartOfDiamnond.toString();
     }
+
+    private boolean isOddNumber(int n) {
+        if (n % 2 != 0) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("This is not odd number! Please try again with any odd number.");
+        }
+    }
+
 
     @Override
     public String drawRectangleTriangle(int n) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <= n; i++) {
+            for (int a = 0; a < i; a++) {
+                stringBuilder.append(STAR);
+            }
+            stringBuilder.append(NEW_LINE);
+        }
+        return stringBuilder.toString();
     }
+
 }
